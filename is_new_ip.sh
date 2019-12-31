d0e2b798bbfc7cb217589cb1888e3476915c1e81
jmd <jmd@mail.example.com>
2019-12-31 20:01:27 +0100

    Cron job checking for new IP

---
+++ is_new_ip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IP=$(curl -s 'https://api.ipify.org?format=json' | jq '.ip')
+
+if [[ -f $HOME/tmp/previous_ip ]]; then
+	OLD_IP=$(cat $HOME/tmp/previous_ip)
+	if [[ $OLD_IP == $IP ]]; then
+		echo "$IP is still valid"
+	else
+		echo $IP >  $HOME/tmp/previous_ip 
+		echo "Sending new IP: $IP"
+		$HOME/bin/pj1_send_jmd_wan_ok.sh
+	fi
+else
+	echo "Sending new IP: $IP"
+	echo $IP >  $HOME/tmp/previous_ip 
+	$HOME/bin/pj1_send_jmd_wan_ok.sh
+fi
+
