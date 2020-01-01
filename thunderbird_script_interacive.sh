52e0575cf552a3be0ad2f92eb38499b1fa9e8de2
jmd <jmd@mail.example.com>
2020-01-01 15:17:39 +0100

    thunderbird script probleme: it is interactive

---
+++ thunderbird_script_interacive.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+thunderbird -compose from='georgesdjmd@gmail.com',to='philtjmd@gmail.com',subject='dinner',body='How about dinner tonight?'
+# sleep 2                  # Wait for the window to open
+# xdotool mousemove 55 105 # Find the exact position of the send button manually
+# sleep 0.25               # Might not be needed
+# xdotool click 1          # Click it
